3eaa0569897d8047547e6cd84059e3e2c5e2ebb9
Soenke Ruempler <soenke@ruempler.eu>
2024-03-26 10:23:02 +0100

    feat: display progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0);
+  const [percentCompleted, setPercentCompleted] = useState(0);
+
+  const initialized = useRef(false);
 
   useEffect(() => {
-    // retrieve items from aws bulk pricing api
+    if (initialized.current) {
+      return;
+    }
+
+    initialized.current = true;
 
     (async () => {
       try {
@@ -19,6 +25,7 @@ function App() {
                 (progressEvent.loaded / progressEvent.total!) * 100,
               );
               console.log("completed: ", percentCompleted);
+              setPercentCompleted(percentCompleted);
             },
           },
         );
@@ -32,25 +39,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <progress max={100} value={percentCompleted} className="sr-only" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
